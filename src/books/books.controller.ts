64fb47b615f8dcbb9c1044f6d3a9bd97d4e10d3a
Deborah Espíndola <deborah.espindola@hotmail.com>
2022-08-18 13:09:36 -0300

    feat(books): add create book endpoint
    Signed-off-by: Deborah Espíndola <deborah.espindola@hotmail.com>

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { CreateBookDto } from './dto/book.dto';
 import { Book } from './interfaces/book.interface';
 
 @Controller('books')
 export class BooksController {
   constructor(private booksService: BooksService) {}
+
+  @Post()
+  createBook(@Body() createBookDto: CreateBookDto) {
+    return this.booksService.createBook(createBookDto);
+  }
 
   @Get()
   getAllBooks(): Book[] {
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { CreateBookDto } from './dto/book.dto';
 import { Book } from './interfaces/book.interface';
 
 @Injectable()
 export class BooksService {
   private readonly books: Book[] = [];
+
+  createBook(book: CreateBookDto) {
+    const newBook = {
+      id: uuid(),
+      ...book,
+    };
+
+    this.books.push(newBook);
+
+    return newBook;
+  }
 
   getAllBooks(): Book[] {
     return this.books;
